c438cb4db0bf436384ae78eb4db85eedc4d26de5
Dennismwendwa <dennismusembi2@gmail.com>
2023-09-20 13:13:52 +0300

    updated console

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -32,15 +32,21 @@ class DBStorage:
 
     def __init__(self):
         """creates new dbstorage instances"""
+        HBNB_MYSQL_USER = getenv('HBNB_MYSQL_USER')
+        HBNB_MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
+        HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
+        HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
+
         self.__engine = create_engine(
                 'mysql+mysqldb://{}:{}@{}/{}'
-                .format(getenv('HBNB_MYSQL_USER'),
-                        getenv('HBNB_MYSQL_PWD'),
-                        getenv('HBNB_MYSQL_HOST', 'localhost'),
-                        getenv('HBNB_MYSQL_DB')),
+                .format(HBNB_MYSQL_USER,
+                        HBNB_MYSQL_PWD,
+                        HBNB_MYSQL_HOST,
+                        HBNB_MYSQL_DB),
                 pool_pre_ping=True
         )
-
+        HBNB_ENV = getenv('HBNB_ENV')
+       
         if getenv('HBNB_ENV') == 'test':
             Base.metadata.drop_all(self.__engine)
 
