63e7d06925b1c6f0b4dc9d4c6b95e2bd4e9b67b9
Dennismwendwa <dennismusembi2@gmail.com>
2023-10-05 12:21:39 +0300

    pycodestyle

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,9 +5,10 @@ from fabric.decorators import runs_once
 from fabric.api import local
 from datetime import datetime
 
+
 @runs_once
 def do_pack():
-    """this func generates archive file"""#
+    """this func generates archive file"""
 
     local("mkdir -p versions")
     path = ("versions/web_static_{}.tgz".format(
